72b7eabee378f18896d348eaac653084e2b34ee0
arelyquinonez <quinonez00@hotmail.com>
2023-06-19 14:17:23 -0600

    home

---
+++ components/home/ContacUs.jsx
@@ -0,0 +1,27 @@
+
+    <div class="lg:mx-32 mx-6 lg:py-24 py-6 text-lg">
+        <div class="flex justify-center">
+            <div class="flex">
+                <h3 class="text-6xl font-light mb-10">Contact</h3>
+                <h3 class="text-6xl font-light mb-10 ml-3"><strong> Us</strong></h3>
+            </div>
+        </div>
+        <div class="grid lg:grid-cols-2 grid-cols-1 lg:gap-10 gap-3 lg:w-3/5 w-4/5 mx-auto block">
+            <div>
+                <h3 class="font-medium py-3">Name</h3>
+                <input class="border border-solid border-gray-300 rounded w-full h-10" type="text" />
+            </div>
+            <div>
+                <h3 class="font-medium py-3">Email</h3>
+                <input class="border border-solid border-gray-300 rounded w-full h-10" type="email" />
+            </div>
+
+        </div>
+        <div class="lg:w-3/5 w-4/5 mx-auto block">
+            <h3 class="font-medium py-3">Message</h3>
+            <textarea class="border border-solid border-gray-300 rounded w-full" cols="30" rows="6"></textarea>
+            <div class="py-6">
+                <button class="border border-solid border-[#b0a49c] h-12 w-24 rounded hover:text-white hover:bg-[#b0a49c] font-normal tracking-wide">Send</button>
+            </div>
+        </div>
+    </div>
