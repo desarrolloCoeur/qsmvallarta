3b7e8cebdaffaf48cb348c0ee920c5bd6bea8c19
arelyquinonez <quinonez00@hotmail.com>
2023-06-30 14:10:48 -0600

    actualizacion galeria

--- app/work-in-progress/page.jsx
+++ app/work-in-progress/page.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+// import Image from 'next/image'
+import Tab from '@/app/work-in-progress/tabs'
 
 const page = () => {
   return (
     <div>
-        <h1>Avance de Obra...</h1>
+      <h1 class="my-12 text-center lg:text-7xl text-3xl font-semibold">Avance de Obra</h1>
+      <Tab />
     </div>
   )
 }
