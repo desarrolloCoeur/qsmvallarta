b85eda6af42aac395d75758dc42cb0ec878e6b89
arelyquinonez <quinonez00@hotmail.com>
2023-07-18 13:06:28 -0600

    menu desplegable

--- components/ui/Header.jsx
+++ components/ui/Header.jsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import { FaBars } from "react-icons/fa";
 import Links from "./Links";
 
+
 const Header = () => {
 
     const [MenuOpen, setMenuOpen] = useState(false);
@@ -11,6 +12,12 @@ const Header = () => {
     const toggleMenu = () => {
         setMenuOpen(!MenuOpen);
     };
+
+
+    const toogleMenuWork = ()=>{
+
+    }
+
 
     return (
         <>
--- components/ui/Links.jsx
+++ components/ui/Links.jsx
@@ -1,4 +1,6 @@
+import React, { useState } from "react";
 import Link from "next/link";
+import './Header.css';
 
 const Links = () => {
     const data = [
@@ -17,22 +19,60 @@ const Links = () => {
         {
             title: "Work in Progress",
             url: "/work-in-progress",
+            url2:"/ocean-canal-2-progress"
         },
     ];
+
+    // Estado para controlar si el menú desplegable está abierto o cerrado
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    // Funciones para manejar eventos de hover
+    const handleMouseEnter = () => {
+        setIsMenuOpen(true);
+    };
+
+    const handleMouseLeave = () => {
+        setIsMenuOpen(false);
+    };
 
     return (
         <>
             {data.map((menu, index) => {
-                return (
-                    <div key={index}>
-                        <Link
-                            href={menu.url}
-                            className="text-white uppercase font-semibold text-lg"
+                if (menu.title === "Work in Progress") {
+                    return (
+                        <div
+                            key={index}
+                            onMouseEnter={handleMouseEnter}
+                            onMouseLeave={handleMouseLeave}
+                            className="menu-container"
                         >
-                            {menu.title}
-                        </Link>
-                    </div>
-                );
+                            {/* Agrega un botón para el enlace */}
+                            <button className="text-white uppercase font-semibold text-lg">
+                                {menu.title}
+                            </button>
+                            {isMenuOpen && (
+                                <div className="dropdown-menu py-6 px-6">
+                                    {/* Contenido del menú desplegable */}
+                                    <Link href={menu.url} className="text-white uppercase font-semibold text-sm ">
+                                        Ocean & Canal
+                                    </Link><br/>
+                                    <Link href={menu.url2} className="text-white uppercase font-semibold text-sm ">
+                                        Ocean & Canal 2
+                                    </Link>
+                                    {/* Agrega más enlaces dentro del menú desplegable si es necesario */}
+                                </div>
+                            )}
+                        </div>
+                    );
+                } else {
+                    return (
+                        <div key={index}>
+                            <Link href={menu.url} className="text-white uppercase font-semibold text-lg">
+                                {menu.title}
+                            </Link>
+                        </div>
+                    );
+                }
             })}
         </>
     );
