3237ec9b7751d2eda370ccf165968dbee2552c64
CoeurMkt <desarrollo@coeurmkt.com>
2023-08-21 10:40:47 -0600

    error links

--- components/ui/Links.jsx
+++ components/ui/Links.jsx
@@ -29,13 +29,6 @@ const NavLinks = () => {
         //     url: "/tower-garden",
         // },
         {
-<<<<<<< HEAD
-=======
-            title: "Garden",
-            url: "/tower-garden",
-        },
-        {
->>>>>>> main
             title: "Progress",
             url: "/work-in-progress",
         }
