0fb9568590ac883d3af5811c5337472f0bf4c114
CoeurMkt <desarrollo@coeurmkt.com>
2023-08-24 09:21:49 -0600

    WhatsApp

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,7 @@ import { Inter,Amiri, Noto_Sans } from 'next/font/google'
  
 
 import './globals.css';
+import Whatsapp from '@/components/Whatsapp.jsx';
 
 const inter = Inter({ subsets: ['latin'],  })
 const amiri = Amiri({subsets : ['latin'], weight : ['400'], variable : '--font-amiri'})
@@ -22,6 +23,7 @@ export default function RootLayout({ children }) {
         <Header />
         {children}
         <Footer />
+        <Whatsapp />
       </body>
     </html>
   )
---
+++ components/Whatsapp.jsx
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+import { FaWhatsapp } from 'react-icons/fa';
+
+const Whatsapp = () => {
+  return (
+    <Link href="https://wa.me/523223735781">
+        <div className='fixed bottom-3 right-3 z-10 bg-main text-white flex items-center gap-2 p-3 rounded-md text-xl font-semibold'>
+            <FaWhatsapp />
+            WhatsApp
+        </div>
+    </Link>
+  )
+}
+
+export default Whatsapp
