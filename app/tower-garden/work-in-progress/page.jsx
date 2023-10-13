31ef1293a3fa75336e6c23c6bd965671023f5b5f
Arely Quiñonez <quinonez00@hotmail.com>
2023-10-13 12:12:11 -0600

    actualizacion work-progress oct garden

--- app/tower-garden/work-in-progress/page.jsx
+++ app/tower-garden/work-in-progress/page.jsx
@@ -2,18 +2,37 @@ import Tabs from '@/components/Tabs'
 import React from 'react'
 
 const tab = [
+    // {
+    //     label : 'September',
+    //     images : ['/img/tower-garden/2023/september/garden-september-1.jpg',
+    //             '/img/tower-garden/2023/september/garden-september-2.jpg',
+    //             '/img/tower-garden/2023/september/garden-september-3.jpg',
+    //             '/img/tower-garden/2023/september/garden-september-4.jpg',
+    //             '/img/tower-garden/2023/september/garden-september-5.jpg',
+    //             '/img/tower-garden/2023/september/garden-september-6.jpg',
+    //             '/img/tower-garden/2023/september/garden-september-7.jpg',
+    //             '/img/tower-garden/2023/september/garden-september-8.jpg',
+    //             ]
+    // },
     {
-        label : 'September',
-        images : ['/img/tower-garden/2023/september/garden-september-1.jpg',
-                '/img/tower-garden/2023/september/garden-september-2.jpg',
-                '/img/tower-garden/2023/september/garden-september-3.jpg',
-                '/img/tower-garden/2023/september/garden-september-4.jpg',
-                '/img/tower-garden/2023/september/garden-september-5.jpg',
-                '/img/tower-garden/2023/september/garden-september-6.jpg',
-                '/img/tower-garden/2023/september/garden-september-7.jpg',
-                '/img/tower-garden/2023/september/garden-september-8.jpg',
+      label : 'October',
+      images : ['/img/tower-garden/2023/october/garden-oct-1.webp',
+                '/img/tower-garden/2023/october/garden-oct-2.webp',
+                '/img/tower-garden/2023/october/garden-oct-3.webp',
+                '/img/tower-garden/2023/october/garden-oct-4.webp',
+                '/img/tower-garden/2023/october/garden-oct-5.webp',
+                '/img/tower-garden/2023/october/garden-oct-6.webp',
+                '/img/tower-garden/2023/october/garden-oct-7.webp',
+                '/img/tower-garden/2023/october/garden-oct-8.webp',
+                '/img/tower-garden/2023/october/garden-oct-9.webp',
+                '/img/tower-garden/2023/october/garden-oct-10.webp',
+                '/img/tower-garden/2023/october/garden-oct-11.webp',
+                '/img/tower-garden/2023/october/garden-oct-12.webp',
+                '/img/tower-garden/2023/october/garden-oct-13.webp',
+                '/img/tower-garden/2023/october/garden-oct-14.webp',
+                '/img/tower-garden/2023/october/garden-oct-15.webp',
                 ]
-    },
+  },
 ]
 
 
