96043a51de484571050a411ae58c3b7f103140a0
arelyquinonez <quinonez00@hotmail.com>
2023-12-13 14:28:49 -0700

    work progress ocean-canal-1

---
+++ app/ocean-and-canal/work-in-progress/page.jsx
@@ -0,0 +1,41 @@
+import Tabs from "@/components/Tabs"
+
+const tab = [
+    {
+        label : 'Early',
+        images : ['/img/work-in-progress/2020/early/avance-1.jpg',
+        '/img/work-in-progress/2020/early/avance-2.jpg',
+        '/img/work-in-progress/2020/early/avance-3.jpg',
+        '/img/work-in-progress/2020/early/avance-4.jpg',
+        '/img/work-in-progress/2020/early/avance-6.jpg']
+    },
+    {
+        label : 'Octubre',
+        images : ['/img/work-in-progress/2020/octubre/working-october.jpg',
+        '',]
+    },
+    {
+        label : 'Noviembre',
+        images : ['/img/work-in-progress/2020/noviembre/wip-noviembre-1.jpg',
+        '',]
+    },
+    {
+        label : 'Diciembre',
+        images : ['/img/work-in-progress/2020/diciembre/wip-diciembre-1.jpg',
+        '',]
+    },
+]
+
+const page = () => {
+    return (
+        <div className="pt-32 container mx-auto w-11/12">
+            <h1 className="text-center lg:text-6xl text-4xl font-semibold">Work in Progress <br/> Ocean & Canal 1</h1>
+            <p className="text-xl font-bold uppercase">Year: 2020</p>
+            <Tabs tabs={tab}/>
+        </div>
+
+
+    )
+}
+
+export default page
